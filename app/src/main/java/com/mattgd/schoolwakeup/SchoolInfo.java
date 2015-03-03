03d19b924d1853e58473ef96a809ccd133b3c77e
mattgd <matt.dzwonczyk@live.com>
2015-03-03 12:03:39 -0500

    Added SchoolInfo class.

---
+++ app/src/main/java/com/mattgd/schoolwakeup/SchoolInfo.java
@@ -0,0 +1,24 @@
+package com.mattgd.schoolwakeup;
+
+/**
+ * Created by mdzwonczyk on 3/3/2015.
+ */
+public class SchoolInfo {
+
+    SchoolInfo(String name, String id) {
+
+    }
+
+    String getSchoolStartTime() {
+        String startTime = "";
+
+        return startTime;
+    }
+
+    String getSchoolMessage() {
+        String message = "";
+
+        return message;
+    }
+
+}
